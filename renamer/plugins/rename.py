d3be6512651eb9ee008565d09ea642427e497090
Judhistir <70930235+judhistir88@users.noreply.github.com>
2021-07-18 09:02:03 +0530

    Update rename.py

--- renamer/plugins/rename.py
+++ renamer/plugins/rename.py
@@ -115,7 +115,7 @@ async def media(c, m):
     try:
         await send_message.edit(TEXT.UPLOAD_START)
         if trace_msg:
-            await trace_msg.edit(f'**User Name:** {m.from_user.mention(style="md")}\n\n**User Id:** `{m.from_user.id}`\n\n**New File Name:** `{new_file_name}`\n\n**Status:** Uploading')
+            await trace_msg.edit(f'**User Name:** {m.from_user.mention(style="md")}\n\n**User Id:** `{m.from_user.id}`\n\n**New File Name:** `{new_file_name}`\n\n**Status:** Uploading ⬆️')
     except:
         pass
 
